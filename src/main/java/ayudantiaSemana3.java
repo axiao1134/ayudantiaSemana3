3a42e8ed579a70ac1ae409990f0f7fbeba789362
Xiao <z.xiao01@ufromail.cl>
2023-08-31 16:12:50 -0400

    commit 2

--- src/main/java/ayudantiaSemana3.java
+++ src/main/java/ayudantiaSemana3.java
@@ -1,14 +1,17 @@
 class ayudantiasemana3 {
     public static void main(String[] args) {
-        String textoOriginal="aaaaa aaaaa";
-        System.out.println(palindromo(textoOriginal));
+        double[] nota1={6.0,5.5,6.4,6.2,5.9};
+        System.out.println(calcularPromedio(nota1));
 
     }
-    public static boolean palindromo(String palabraIngresada) {
-        palabraIngresada = palabraIngresada.replace(" ", "");
-        StringBuilder stringBuilder = new StringBuilder(palabraIngresada);
-        String palabraInvertida = stringBuilder.reverse().toString();
-        return palabraIngresada.equalsIgnoreCase(palabraInvertida);
+    public static double calcularPromedio(double [] notas) {
+        double [] ponderacionNotas = {0.25, 0.25, 0.25, 0.15, 0.10};
+        double promedio = 0;
+        for (int i = 0; i < notas.length; i++) {
+            promedio += (notas[i] * ponderacionNotas[i]);
+        }
+        return promedio;
     }
+
 
 }
--- src/test/java/ayudantiaSemana3Test.java
+++ src/test/java/ayudantiaSemana3Test.java
@@ -1,15 +1,20 @@
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
-class ayudantiasemana3Test {
+class ayudantiasemana3ExtremeTest {
 
-    @org.junit.jupiter.api.Test
-    void pruebaPalindromo() {
-        assertTrue(ayudantiasemana3.palindromo("oso"));
-        assertTrue(ayudantiasemana3.palindromo("reconocer"));
-        assertTrue(ayudantiasemana3.palindromo("A man a plan a canal Panama"));
+    @Test
+    void pruebaCalcularPromedioConNotasCero() {
+        double[] notas = {0.0, 0.0, 0.0, 0.0, 0.0};
+        double resultadoEsperado = 0.0; // Todas las notas son cero, por lo que el promedio debe ser cero
+        assertEquals(resultadoEsperado, ayudantiasemana3.calcularPromedio(notas), 0.0001);
 
-        assertFalse(ayudantiasemana3.palindromo("hola"));
-        assertFalse(ayudantiasemana3.palindromo("programación"));
-        assertFalse(ayudantiasemana3.palindromo("Esta no es un palíndromo"));
+
+    }
+    @Test
+    void pruebaCalcularPromedio() {
+        double[] notas1 = {6.0, 5.5, 6.4, 6.2, 5.9};
+        double resultadoEsperado1 = 6.0 * 0.25 + 5.5 * 0.25 + 6.4 * 0.25 + 6.2 * 0.15 + 5.9 * 0.10;
+        assertEquals(resultadoEsperado1, ayudantiasemana3.calcularPromedio(notas1), 0.0001);
     }
 }
