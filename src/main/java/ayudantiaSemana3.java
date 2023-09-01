6131f7bf70eb04a5d27265de009e5184f0305908
Xiao <z.xiao01@ufromail.cl>
2023-08-31 20:52:08 -0400

    commit 3

--- src/main/java/ayudantiaSemana3.java
+++ src/main/java/ayudantiaSemana3.java
@@ -2,12 +2,12 @@ class ayudantiasemana3 {
     public static void main(String[] args) {
 
     }
-    private static String metodo3(int [] puntajes) {
+    public static String juegoPuntaje(int [] puntajes) {
         int jugador1 = 20 - puntajes[0];
         int jugador2 = 20 - puntajes[1];
         if((jugador1 < 0 && jugador2 < 0) || jugador1 == jugador2) {
             return "Empate.";
-        } else if((jugador1 < 0 || jugador2 < jugador1) && jugador2 >= 0) {
+        } else if((jugador2 < jugador1) && jugador2 >= 0) {
             return "Jugador 2 ganó.";
         } else {
             return "Jugador 1 ganó.";
