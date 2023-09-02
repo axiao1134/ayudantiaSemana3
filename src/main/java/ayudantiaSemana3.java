9a6ae759bb93b9448cc585eec905a406aded90d1
Xiao <z.xiao01@ufromail.cl>
2023-09-01 23:14:37 -0400

    actividad2

--- src/main/java/ayudantiaSemana3.java
+++ src/main/java/ayudantiaSemana3.java
@@ -4,6 +4,11 @@ class AyudantiaSemana3 {
     public static void main(String[] args) {
         int[][] matrizTemperatura = new int[7][24];
         matrizTemperatura = crearTemperaturaAleatoria(matrizTemperatura);
+        int diaMasCaluroso = diaMasCaluroso(matrizTemperatura);
+        int diaMasFrio = diaMasFrio(matrizTemperatura);
+
+        System.out.println("El día más caluroso fue el día " + diaMasCaluroso);
+        System.out.println("El día más frío fue el día " + diaMasFrio);
 
     }
 
@@ -13,11 +18,46 @@ class AyudantiaSemana3 {
         Random ran = new Random();
 
         for (int dia = 0; dia < 7; dia++) {
-            for (int hora = 0; hora < 24; hora++) {
+            for (int hora = 0; hora <24; hora++) {
                 matrizTemperatura[dia][hora] = ran.nextInt(temMax - temMin + 1) + temMin;
             }
         }
 
         return matrizTemperatura;
+    }
+    public static int diaMasCaluroso(int[][] matrixTemperatura){
+        int elDiaMasCaluroso=0;
+        int elGradoCelsiu=0;
+        for (int dia = 0; dia < 7; dia++) {
+            for (int hora = 0; hora <24; hora++) {
+                int diaYHora= matrixTemperatura[dia][hora];
+                if (elGradoCelsiu<diaYHora){
+                    elGradoCelsiu=matrixTemperatura[dia][hora];
+                    elDiaMasCaluroso=dia+1;
+
+                }
+
+            }
+        }
+
+    return elDiaMasCaluroso;
+    }
+    public static int diaMasFrio(int[][] matrixTemperatura){
+        int elDiaMasFrio=0;
+        int elGradoCelsiu=50;
+        for (int dia = 0; dia < 7; dia++) {
+            for (int hora = 0; hora <24; hora++) {
+                int diaYHora= matrixTemperatura[dia][hora];
+                if (elGradoCelsiu>diaYHora){
+                    elGradoCelsiu=matrixTemperatura[dia][hora];
+                    elDiaMasFrio=dia+1;
+
+                }
+
+            }
+        }
+
+        return elDiaMasFrio;
+
     }
 }
