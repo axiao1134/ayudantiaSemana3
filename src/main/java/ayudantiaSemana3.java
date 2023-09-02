b6832e439100cf464cddf598a928354f2450ca97
Xiao <z.xiao01@ufromail.cl>
2023-09-02 00:01:42 -0400

    actividad2

--- src/main/java/ayudantiaSemana3.java
+++ src/main/java/ayudantiaSemana3.java
@@ -9,7 +9,14 @@ class AyudantiaSemana3 {
 
         System.out.println("El día más caluroso fue el día " + diaMasCaluroso);
         System.out.println("El día más frío fue el día " + diaMasFrio);
+        int[] horaYDiaMasFrio = horaYDiaTemperaturaMasBaja(matrizTemperatura);
+        int horaMasFria = horaYDiaMasFrio[0];
 
+        System.out.println("La hora más fría fue a las " + horaMasFria + ":00 horas en el día " + diaMasFrio);
+        int[] horaYDiaMasCaluroso = horaYDiaTemperaturaMasAlta(matrizTemperatura);
+        int horaMasCalurosa = horaYDiaMasCaluroso[0];
+
+        System.out.println("La hora más calurosa fue a las " + horaMasCalurosa + ":00 horas en el día " + diaMasCaluroso);
     }
 
     public static int[][] crearTemperaturaAleatoria(int[][] matrizTemperatura) {
@@ -58,6 +65,45 @@ class AyudantiaSemana3 {
         }
 
         return elDiaMasFrio;
+
+    }
+    public static int[] horaYDiaTemperaturaMasBaja(int[][] matrizTemperatura) {
+        int diaMasFrio = 0;
+        int horaMasFria = 0;
+        int temperaturaMasBaja = matrizTemperatura[0][0]; // Inicializamos con el primer valor
+
+        for (int dia = 0; dia < matrizTemperatura.length; dia++) {
+            for (int hora = 0; hora < matrizTemperatura[dia].length; hora++) {
+                int temperaturaActual = matrizTemperatura[dia][hora];
+                if (temperaturaActual < temperaturaMasBaja) {
+                    temperaturaMasBaja = temperaturaActual;
+                    diaMasFrio = dia + 1;
+                    horaMasFria = hora;
+                }
+            }
+        }
+
+        int[] horaYDiaMasFrio = {horaMasFria, diaMasFrio};
+        return horaYDiaMasFrio;
+    }
+    public static int[] horaYDiaTemperaturaMasAlta(int[][] matrizTemperatura) {
+        int diaMasCaluroso = 0;
+        int horaMasCalurosa = 0;
+        int temperaturaMasAlta = matrizTemperatura[0][0]; // Inicializamos con el primer valor
+
+        for (int dia = 0; dia < matrizTemperatura.length; dia++) {
+            for (int hora = 0; hora < matrizTemperatura[dia].length; hora++) {
+                int temperaturaActual = matrizTemperatura[dia][hora];
+                if (temperaturaActual > temperaturaMasAlta) {
+                    temperaturaMasAlta = temperaturaActual;
+                    diaMasCaluroso = dia + 1;
+                    horaMasCalurosa = hora;
+                }
+            }
+        }
+
+        int[] horaYDiaMasCaluroso = {horaMasCalurosa, diaMasCaluroso};
+        return horaYDiaMasCaluroso;
 
     }
 }
